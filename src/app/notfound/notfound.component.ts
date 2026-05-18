6730175abdc16b205a3993bbee59a1e70c600cd2
Shannon Kueneke <slkueneke@gmail.com>
2026-05-18 15:16:13 -0700

    404 page

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   template: `
-    <p>
-      notfound works!
-    </p>
+    <div class="flexWrapper">
+      <h2 class="font-bubble">404</h2>
+      <p>Hey! What are you doing <em>here</em>?</p>
+      <p>Let's get you <a routerLink="/">home</a>.</p>
+    </div>
   `,
-  styles: ``
-})
-export class NotfoundComponent {
+  styles: `
+    .flexWrapper {
+      display: block;
+    }
 
-}
+    p,
+    h2 {
+      text-align: center;
+      font-size: 1.5em;
+    }
+
+    h2 {
+      font-size:5em;
+    }
+
+    a {
+      text-decoration: underline;
+    }
+
+    .font-bubble {
+      font-family: 'Bungee Shade', sans-serif;
+    }
+  `,
+})
+export class NotfoundComponent {}
